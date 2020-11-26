6522a819b2a658c52b811ef8b222db63ea02b4b8
Umut Aktepe <umtaktpe@gmail.com>
2020-11-26 09:58:05 +0300

    Snapshot, backup and account endpoints added.

--- src/Api/Server.php
+++ src/Api/Server.php
@@ -129,4 +129,130 @@ class Server extends Base
 
         return $this->request("/server/manage/$this->server/ip/rdns", $query);
     }
+
+    /**
+     * Get all snapshots.
+     *
+     * @return array|mixed
+     */
+    public function snapshotList()
+    {
+        return $this->request("/server/manage/$this->server/snapshot");
+    }
+
+    /**
+     * Create a new snapshot.
+     *
+     * @param string $snapshotName
+     * @return array|mixed
+     */
+    public function snapshotCreate($snapshotName)
+    {
+        $query = [
+            'name' => $snapshotName,
+        ];
+
+        return $this->request("/server/manage/$this->server/snapshot/create", $query);
+    }
+
+    /**
+     * @param string $snapshotName
+     * @return array|mixed
+     */
+    public function snapshotRevert($snapshotName)
+    {
+        $query = [
+            'name' => $snapshotName,
+        ];
+
+        return $this->request("/server/manage/$this->server/snapshot/revert", $query);
+    }
+
+    /**
+     * Recreate your snapshots.
+     *
+     * @param string $snapshotName
+     * @return array|mixed
+     */
+    public function snapshotUpdate($snapshotName)
+    {
+        $query = [
+            'name' => $snapshotName,
+        ];
+
+        return $this->request("/server/manage/$this->server/snapshot/update", $query);
+    }
+
+    /**
+     * Delete snapshot.
+     *
+     * @param string $snapshotName
+     * @return array|mixed
+     */
+    public function snapshotDelete($snapshotName)
+    {
+        $query = [
+            'name' => $snapshotName,
+        ];
+
+        return $this->request("/server/manage/$this->server/snapshot/delete", $query);
+    }
+
+    /**
+     * Add cron to your snapshot.
+     *
+     * @param array $query
+     * @return array|mixed
+     */
+    public function snapshotAddCron($snapshotName, $day, $hour, $minute)
+    {
+        $query = [
+            'name' => $snapshotName,
+            'day' => $day,
+            'hour' => $hour,
+            'min' => $minute,
+        ];
+
+        return $this->request("/server/manage/$this->server/snapshot/cron/add", $query);
+    }
+
+    /**
+     * Delete cron.
+     *
+     * @param string $query
+     * @return array|mixed
+     */
+    public function snapshotDeleteCron($snapshotName)
+    {
+        $query = [
+            'name' => $snapshotName,
+        ];
+
+        return $this->request("/server/manage/$this->server/snapshot/cron/delete", $query);
+    }
+
+    /**
+     * Get backup list.
+     *
+     * @return array|mixed
+     */
+    public function backupList()
+    {
+        return $this->request("/server/manage/$this->server/backup");
+    }
+
+    /**
+     * Restore your backup.
+     *
+     * @param string $backupName
+     * @return array|mixed
+     */
+    public function backupRestore($backupName)
+    {
+        $query = [
+            'backup_name' => $backupName,
+        ];
+
+        return $this->request("/server/manage/$this->server/backup/restore", $query);
+    }
 }
